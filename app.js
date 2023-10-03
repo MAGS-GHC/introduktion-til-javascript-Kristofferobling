eccfe7fe1cbb054386aa50c9b2eb226f90dc4426
Kristofferobling <115920215+Kristofferobling@users.noreply.github.com>
2023-10-03 15:59:28 +0200

    Lavet opgave 1.1 -> 1.6
    Opgaverne er lavet undtagen 1.4

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 //Skriv alt jeres kode her
-
+//Opgave 1.1
 console.log(
   "Opgave 1.1 - Lav et program, som skriver “Hello World” i konsollen"
 );
 //Lav jeres svar her
-console.log("XX");
+console.log("Hello World");
+
+//Opgave 1.2
+console.log("Opgave 1.2 - Lav et program, som skriver “Hello” og på næste linje skriver “World” i konsollen");
+console.log("Hello \nworld");
+
+//Opgave 1.3
+console.log("Opgave 1.3 - Lav et program, som skriver følgende i konsollen. →");
+console.log("* \n** \n*** \n** \n*");
+
+//Opgave 1.4
+console.log("Opgave 1.4 - Lav et program, hvor i definere en konstant eller variabel og programmet skriver hvilken type det er, i konsollen. (Altså om det er en int, string, bool osv.)");
+console.log();
+
+//Opgave 1.5
+console.log("Opgave 1.5 - Print svarene på følgende matematiske operationer ved at skrive det direkte ind i Console.log():");
+console.log(10-5);
+console.log(25/3);
+console.log(25%3);
+console.log(25%2);
+console.log(3**2);
+
+//Opgave 1.6
+console.log("Opgave 1.6 - Kombiner 2 strings");
+console.log(tekst1 = "Hello ");
+console.log(tekst2 = "World!");
