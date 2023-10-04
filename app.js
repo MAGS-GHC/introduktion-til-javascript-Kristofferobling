7ecb0e46239777cf016c3e0b538239c10e640006
Kristofferobling <115920215+Kristofferobling@users.noreply.github.com>
2023-10-04 11:11:30 +0200

    Opgave 1 & 2 er færdig
    Opgaverne til og med 2.6 er færdig

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ console.log("* \n** \n*** \n** \n*");
 
 //Opgave 1.4
 console.log("Opgave 1.4 - Lav et program, hvor i definere en konstant eller variabel og programmet skriver hvilken type det er, i konsollen. (Altså om det er en int, string, bool osv.)");
-console.log();
+let x = "5";
+console.log(typeof x);
 
 //Opgave 1.5
 console.log("Opgave 1.5 - Print svarene på følgende matematiske operationer ved at skrive det direkte ind i Console.log():");
@@ -28,5 +29,28 @@ console.log(3**2);
 
 //Opgave 1.6
 console.log("Opgave 1.6 - Kombiner 2 strings");
-console.log(tekst1 = "Hello ");
-console.log(tekst2 = "World!");
+let tekst1 = "Hello "
+let tekst2 = "World!"
+console.log(tekst1 + tekst2);
+
+
+//Opgave 1.7
+let gf2online = "GF2 Online"
+console.log(gf2online.length);
+
+//Opgave 1.8
+console.log(Math.random());
+
+//Opgave 1.9
+let number = 23
+
+console.log(number)
+console.log(number = "23")
+console.log(number == "23")
+console.log(number === "23")
+console.log(number -= 23)
+console.log(number += 23)
+console.log(number += "23")
+console.log(number -= "46")
+console.log(number != "23")
+console.log(number **= 2)
